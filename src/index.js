4c5e1984fb87a39476dd0f53824cf1135a52b55e
Valentyn Kovalchuk <kovalhood@gmail.com>
2023-05-25 20:52:47 +0300

    Added favicon

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store.store}>
       <PersistGate loading={null} persistor={store.persistor}>
-        {/* <BrowserRouter basename="/"> */}
         <BrowserRouter basename="/delivery-app">
           <App />
         </BrowserRouter>
